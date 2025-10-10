8d424a0cd63c3a2eb1f36e5d565bb373c247d0bb
Yuliya Karimova <ykarimovao@ozon.ru>
2025-10-11 00:40:57 +0500

    fix: custom theme

---
+++ site/assets/js/main.js
@@ -0,0 +1,5 @@
+// Пример минимальной прогрессивной функциональности
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll(".content a[href^='http']");
+  links.forEach(a => a.setAttribute("target", "_blank"));
+});
